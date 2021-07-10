5c649436397ad278a7f4ed015fb09cc28d5bf27f
Subbotina Eugenia <63358524+jenyasubbotina@users.noreply.github.com>
2021-07-11 00:01:51 +0500

    Initial commit

---
+++ app/src/main/java/com/jenyasubbotina/adoptme/ui/home/MailsFragment.java
@@ -0,0 +1,38 @@
+package com.jenyasubbotina.adoptme.ui.home;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jenyasubbotina.adoptme.R;
+
+public class MailsFragment extends Fragment {
+
+    private MailsViewModel mViewModel;
+
+    public static MailsFragment newInstance() {
+        return new MailsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mails, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MailsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/jenyasubbotina/adoptme/ui/home/MailsViewModel.java
@@ -0,0 +1,7 @@
+package com.jenyasubbotina.adoptme.ui.home;
+
+import androidx.lifecycle.ViewModel;
+
+public class MailsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
