7a722ce81a26f9dd08ab6cae3a35f0c52ce9008c
Subbotina Eugenia <63358524+jenyasubbotina@users.noreply.github.com>
2021-07-23 23:22:55 +0500

    Icon added

---
+++ app/src/main/java/com/jenyasubbotina/adoptme/ui/activities/AuthActivity.java
@@ -0,0 +1,16 @@
+package com.jenyasubbotina.adoptme.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jenyasubbotina.adoptme.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
